7b7635b660d4cc896abc7fdb07059f8c4b37afca
Shaema <huducn@gmail.com>
2023-06-28 13:21:51 +0300

    Added translation for login and registration partials

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -69,7 +69,7 @@ body {
 <div class="container">
   
     <form class="login-form" method="post" action="{{ route('login.perform') }}">
-      <h2>Login</h2>
+      <h2>{{ __('Login') }}</h2>
 
       <input type="hidden" name="_token" value="{{ csrf_token() }}" />
         
@@ -77,8 +77,8 @@ body {
         @include('layouts.partials.messages')
 
         <div class="form-group form-floating mb-3">
-        <label for="floatingName">Username</label>
-            <input type="text" class="form-control" name="username" value="{{ old('username') }}" placeholder="Username" required="required" autofocus>
+        <label for="floatingName">{{ __('Username') }}</label>
+            <input type="text" class="form-control" name="username" value="{{ old('username') }}" placeholder="{{ __('Username') }}" required="required" autofocus>
            
             @if ($errors->has('username'))
                 <span class="text-danger text-left">{{ $errors->first('username') }}</span>
@@ -86,16 +86,16 @@ body {
         </div>
         
         <div class="form-group form-floating mb-3">
-        <label for="floatingPassword">Password</label>
-            <input type="password" class="form-control" name="password" value="{{ old('password') }}" placeholder="Password" required="required">
+        <label for="floatingPassword">{{ __('Password') }}</label>
+            <input type="password" class="form-control" name="password" value="{{ old('password') }}" placeholder="{{ __('Password') }}" required="required">
             
             @if ($errors->has('password'))
                 <span class="text-danger text-left">{{ $errors->first('password') }}</span>
             @endif
         </div>
 
-        <button class="w-100 btn btn-lg btn-primary" type="submit">Login</button>
-        <p><a href="#">Forgot password?</a></p>
+        <button class="w-100 btn btn-lg btn-primary" type="submit">{{ __('Login') }}</button>
+        <!-- <p><a href="#">Forgot password?</a></p> -->
         @include('auth.partials.copy')
 
 
